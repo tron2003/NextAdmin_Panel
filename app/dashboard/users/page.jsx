baf7c2e069de76b4ae18bbe288bb613a90dbc257
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-05-14 02:34:55 +0530

    first commit

---
+++ app/dashboard/users/page.jsx
@@ -0,0 +1,7 @@
+const Users = () => {
+
+    return (
+        <div className="">Users</div>
+    )
+}
+export default Users
---
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -0,0 +1,8 @@
+import styles from "./siebar.module.css"
+const Sidebar = () => { 
+
+    return (
+        <div className={ styles.container}>Sidebar</div>
+    )
+}
+export default Sidebar
